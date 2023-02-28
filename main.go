cd7efc25bfade5e06cf769744a3f5a57413dc605
Prashant Bhuruk <prashantbhuruk88@gmail.com>
2023-02-28 01:00:59 -0800

    add constraint

--- main.go
+++ main.go
@@ -2,6 +2,10 @@ package main
 
 import "fmt"
 
+type Number interface {
+	int64 | float64
+}
+
 func SumInts(m map[string]int64) int64 {
 	var s int64
 	for _, v := range m {
@@ -26,6 +30,14 @@ func SumIntsOrFloats[K comparable, V int64 | float64](m map[K]V) V {
 
 }
 
+func SumNumbers[K comparable, V Number](m map[K]V) V {
+	var s V
+	for _, v := range m {
+		s += v
+	}
+	return s
+}
+
 func main() {
 	ints := map[string]int64{
 		"first":  19,
@@ -46,5 +58,9 @@ func main() {
 	fmt.Printf("Generic Sums type inferred: %v and %v \n",
 		SumIntsOrFloats(ints),
 		SumIntsOrFloats(floats))
+
+	fmt.Printf("Generic Sums with constraint: %v and %v \n",
+		SumNumbers(ints),
+		SumNumbers(floats))
 
 }
