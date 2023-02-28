f9158177a818f4139dedeceb2f9b1e5c8fd7db9b
Prashant Bhuruk <prashantbhuruk88@gmail.com>
2023-02-28 00:54:21 -0800

    add generic function

--- main.go
+++ main.go
@@ -17,6 +17,15 @@ func SumFloats(m map[string]float64) float64 {
 	return s
 }
 
+func SumIntsOrFloats[K comparable, V int64 | float64](m map[K]V) V {
+	var s V
+	for _, v := range m {
+		s += v
+	}
+	return s
+
+}
+
 func main() {
 	ints := map[string]int64{
 		"first":  19,
@@ -29,5 +38,13 @@ func main() {
 	fmt.Printf("Non Generic Sum: %v and %v \n",
 		SumInts(ints),
 		SumFloats(floats))
+
+	fmt.Printf("Generic Sums: %v and %v \n",
+		SumIntsOrFloats[string, int64](ints),
+		SumIntsOrFloats[string, float64](floats))
+
+	fmt.Printf("Generic Sums type inferred: %v and %v \n",
+		SumIntsOrFloats(ints),
+		SumIntsOrFloats(floats))
 
 }
